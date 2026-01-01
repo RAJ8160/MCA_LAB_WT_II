a15b0672b3665a8d012700744c9e59aa5ad1337f
Kavyam Joshi <161846215+kavyam-github@users.noreply.github.com>
2026-01-01 08:55:38 +0530

    Update path.js

--- Lab_5/path.js
+++ Lab_5/path.js
@@ -1,3 +1,4 @@
+//The path module is a built-in (core) Node.js module used to work with file and directory paths in a platform-independent way (Windows, Linux, macOS).
 const path = require('path')
 console.log('=============================================\n')
 console.log("Directory(Folder) Name is",__dirname);
@@ -10,12 +11,15 @@ console.log(filePath);
 
 // You can pass String in below functions
 // str = "MyFolder/MyFile1/MyFile2/MyFile3.txt"
-const parseData = path.parse(filePath)
+const parseData = path.parse(filePath)// Returns an object with path details.
 const resolvedPath = path.resolve(filePath)//Provide absolute path from beginning
-const baseName = path.basename(filePath)
-const dirName = path.dirname(filePath)
+const baseName = path.basename(filePath) // Returns the last portion of a path (file name).
+const dirName = path.dirname(filePath) //Returns the directory name of a path.
+const fileExtension = path.extname(filePath) //Returns the file extension.
 
 console.log("Parse Data:\n",parseData)
 console.log("ResolvedPath is:\n",resolvedPath)
 console.log("BaseName is:\n",baseName)
+console.log("Extension is : \n",fileExtension)
+
 console.log("Directory Name is:",dirName)
